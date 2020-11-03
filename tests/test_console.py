d0b7f4602881c1a282528ea90db72c84404b7df7
Liu Xiaodong <liuxd2018@foxmail.com>
2020-11-03 10:59:51 +0800

    add tests

---
+++ tests/test_console.py
@@ -0,0 +1,22 @@
+import click.testing
+import pytest
+
+from modern_python import console
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
+
+def test_main_succeeds(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
+    assert "Lorem Ipsum" in result.output
