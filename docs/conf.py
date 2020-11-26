aec85ab8e0ecd5b1ddc8d1665decf97576d01733
Liu Xiaodong <liuxd2018@foxmail.com>
2020-11-26 22:17:35 +0800

    generation API doc using sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -2,3 +2,8 @@
 project = "modern-python"
 author = "liu xiaodong"
 copyright = f"2020, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
