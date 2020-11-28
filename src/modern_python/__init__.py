94ace2cd3d6ab1584a8a6284f9caf7f29072807b
Liu Xiaodong <liuxd2018@foxmail.com>
2020-11-28 17:19:31 +0800

    single sourcing package version

--- src/modern_python/__init__.py
+++ src/modern_python/__init__.py
@@ -1,2 +1,8 @@
 """Themodern Python project."""
-__version__ = "0.3.2"
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
